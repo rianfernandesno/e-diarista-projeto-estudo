e383d40b944e8aef02f7e3dafdf99b3eae91ae6b
rianfernandesno <zrpapz12@gmail.com>
2025-08-16 19:33:48 -0300

    "Enum: Icone"

---
+++ src/main/java/com/yui/e_diarista/core/enums/Icone.java
@@ -0,0 +1,18 @@
+package com.yui.e_diarista.core.enums;
+
+public enum Icone {
+
+    TWF_CLEANING_1("twf-cleaning-1"),
+    TWF_CLEANING_2("twf-cleaning-2"),
+    TWF_CLEANING_3("twf-cleaning-3");
+
+    private String nome;
+
+    private Icone(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
