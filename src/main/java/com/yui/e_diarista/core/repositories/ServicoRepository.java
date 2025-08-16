fe85c09a7488c00aa56b0a5600b9e50067df40a2
rianfernandesno <zrpapz12@gmail.com>
2025-08-16 19:38:00 -0300

    "Repository: ServicoRepository"

---
+++ src/main/java/com/yui/e_diarista/core/repositories/ServicoRepository.java
@@ -0,0 +1,7 @@
+package com.yui.e_diarista.core.repositories;
+
+import com.yui.e_diarista.core.models.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Long> {
+}
