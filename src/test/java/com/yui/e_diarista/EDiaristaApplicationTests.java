921cb9101e2271b1a3824beeb3e2456ea254ec00
rianfernandesno <zrpapz12@gmail.com>
2025-08-16 17:50:04 -0300

    first commit

---
+++ src/test/java/com/yui/e_diarista/EDiaristaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yui.e_diarista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EDiaristaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
